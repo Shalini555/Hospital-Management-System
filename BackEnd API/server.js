f0ac444202e27743ec9cd94b02cf93ef74cc192c
it19051680@my.sliit.lk <49786108+RavinduDeshan@users.noreply.github.com>
2023-07-20 20:51:50 +0530

    back end init

---
+++ BackEnd API/server.js
@@ -0,0 +1,47 @@
+// server.js
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 8080;
+const cors = require("cors");
+const session = require("express-session");
+const fileupload = require("express-fileupload");
+
+const mongoose = require("mongoose");
+
+const config = require("./configure.js");
+
+const userRoute = require("./Route/user.router");
+const companyRoute = require("./Route/company.router");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(config.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database is connected");
+    },
+    (err) => {
+      console.log("Can not connect to the database" + err);
+    }
+  );
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  fileupload({
+    useTempFiles: true,
+  })
+);
+
+app.use("/company", companyRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, function () {
+  console.log("Server is running on Port:", PORT);
+});
