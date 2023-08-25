40c44503714947cb110710b48b805904d17b21fc
Sahan Kariyawasam <68751902+sahan-98@users.noreply.github.com>
2023-08-25 10:46:20 +0530

    update

--- BackEnd_API/Route/bed.router.js
+++ BackEnd_API/Route/bed.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bedRoutes = express.Router();
 const bcrypt = require("bcrypt");
+const axios = require("axios");
 
 const moment = require("moment");
 // const session = require("express-session");
@@ -25,12 +26,12 @@ bedRoutes.post("/add", async (req, res) => {
     bedFee,
   } = req.body;
 
-  const exist = await Bed.findOne({ patientid: patientid });
-  if (exist) {
-    return res
-      .status(202)
-      .json({ warn: "A bed is Exist with this patient Id" });
-  }
+  // const exist = await Bed.findOne({ patientid: patientid });
+  // if (exist) {
+  //   return res
+  //     .status(202)
+  //     .json({ warn: "A bed is Exist with this patient Id" });
+  // }
 
   if (bedNo == "" || wardNo == "" || bedFee == "")
     return res.status(202).json({ warn: "Important field(s) are empty" });
@@ -38,7 +39,7 @@ bedRoutes.post("/add", async (req, res) => {
   const newBed = new Bed({
     bedNo,
     patientid,
-    availability: true,
+    availability,
     wardNo,
     allocatedDate,
     releaseDate,
@@ -57,10 +58,9 @@ bedRoutes.post("/add", async (req, res) => {
 });
 
 // bed manual allocate
-bedRoutes.post("/allocateBed/:id", async (req, res) => {
-  console.log(req.body);
+bedRoutes.post("/allocateBed", async (req, res) => {
   try {
-    const { patientid } = req.body;
+    const { patientid, bedNo, wardNo } = req.body;
 
     const exist = await Bed.findOne({ patientid: patientid });
     if (exist) {
@@ -68,8 +68,12 @@ bedRoutes.post("/allocateBed/:id", async (req, res) => {
         .status(202)
         .json({ warn: "A bed is Exist with this patientId" });
     }
-    Bed.findById(req.params.id)
-      .then((bedObj) => {
+    Bed.find({ bedNo: bedNo, wardNo: wardNo, availability: true })
+      .then((bedObjs) => {
+        if (bedObjs.length === 0) {
+          return res.status(202).json({ warn: "No bed available" });
+        }
+        const bedObj = bedObjs[0];
         bedObj.patientid = req.body.patientid;
         bedObj.availability = false;
         bedObj.allocatedDate = req.body.allocatedDate;
@@ -79,7 +83,10 @@ bedRoutes.post("/allocateBed/:id", async (req, res) => {
           .then(() => {
             return res.status(200).json("allocated");
           })
-          .catch((err) => res.status(400).json({ message: err }));
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json({ message: err });
+          });
       })
       .catch((err) => res.status(400).json({ message: err }));
   } catch (error) {
@@ -206,6 +213,66 @@ bedRoutes.route("/release-all-beds").post(async function (req, res) {
 
     return res.status(500).json({ message: "Server Error" });
   }
+});
+
+//estimate bed stay days
+bedRoutes.post("/stayDays", async (req, res) => {
+  await axios
+    .post(`http://127.0.0.1:5001/predict`, {
+      Available_Extra_Rooms_in_Hospital:
+        req.body.Available_Extra_Rooms_in_Hospital,
+      staff_available: req.body.staff_available,
+      Visitors_with_Patient: req.body.Visitors_with_Patient,
+      Admission_Deposit: req.body.Admission_Deposit,
+      Ward_Facility_Code_B: req.body.Ward_Facility_Code_B,
+      Ward_Facility_Code_C: req.body.Ward_Facility_Code_C,
+      Ward_Facility_Code_D: req.body.Ward_Facility_Code_D,
+      Ward_Facility_Code_E: req.body.Ward_Facility_Code_E,
+      Ward_Facility_Code_F: req.body.Ward_Facility_Code_F,
+      Age_20_Nov: req.body.Age_20_Nov,
+      Age_21_30: req.body.Age_21_30,
+      Age_31_40: req.body.Age_31_40,
+      Age_41_50: req.body.Age_41_50,
+      Age_51_60: req.body.Age_51_60,
+      Age_61_70: req.body.Age_61_70,
+      Age_71_80: req.body.Age_71_80,
+      Age_81_90: req.body.Age_81_90,
+      Age_91_100: req.body.Age_91_100,
+      gender_Male: req.body.gender_Male,
+      gender_Other: req.body.gender_Other,
+      Type_of_Admission_Trauma: req.body.Type_of_Admission_Trauma,
+      Type_of_Admission_Urgent: req.body.Type_of_Admission_Urgent,
+      Severity_of_Illness_Minor: req.body.Severity_of_Illness_Minor,
+      Severity_of_Illness_Moderate: req.body.Severity_of_Illness_Moderate,
+      Insurance_Yes: req.body.Insurance_Yes,
+    })
+    .then(async (flaskRes) => {
+      console.log("==========Flask Response is============");
+      console.log(flaskRes.data.predicted_stay);
+      console.log("====================================");
+
+      try {
+        const estimateValue = flaskRes.data.predicted_stay.toString();
+        const bedid = req.body._id;
+        const bedObj = await Bed.findOne({ _id: bedid });
+
+        if (!bedObj) {
+          return res.status(404).json({ message: "Bed not found" });
+        }
+
+        bedObj.estimation = estimateValue;
+
+        await bedObj.save();
+        return res.status(200).json({ data: bedObj });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server Error" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json("Error " + err);
+    });
 });
 
 // get patient for bed details
